181c1815f91fbc414b5b707e3915e0734d315e6d
Benedikt Schesch <b.schesch@gmail.com>
2025-07-15 01:27:24 -0700

    Updated dataset

--- eval_all_models.sh
+++ eval_all_models.sh
@@ -3,7 +3,9 @@
 set -e
 
 # Default maximum number of parallel workers for eval.py
-MAX_WORKERS=${1:-4}
+MAX_WORKERS=${1:-128}
+
+MAX_SAMPLES=${2:-50}
 
 # List of all available languages from merges/ directory
 LANGUAGES=(
@@ -38,7 +40,7 @@ for language in "${LANGUAGES[@]}"; do
     for model in "${MODELS[@]}"; do
         current_job=$((current_job + 1))
         echo "[$current_job/$total_jobs] Evaluating $model on $language"
-        python3 eval.py --model_name "$model" --language "$language" --max_workers "$MAX_WORKERS"
+        python3 eval.py --model_name "$model" --language "$language" --max_samples "$MAX_SAMPLES" --max_workers "$MAX_WORKERS" --verbose
     done
 done
 
